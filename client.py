319ff6e6d50c2bcbaae041b8df8cff64b464c2f4
VCM admin account <vcm@vcm-3586.vm.duke.edu>
2018-03-09 10:34:52 -0500

    complete client

--- client.py
+++ client.py
@@ -1,4 +1,32 @@
 import requests
-#r = requests.get("http://127.0.0.1:5000/name")
-r = requests.get("http://vcm-3586.vm.duke.edu:5000/name")
-print(r.json())
+
+def get_name():
+    """makes a get request and returns the name"""
+    r  = requests.get("http://vcm-3586.vm.duke.edu:5000/name")
+    name = r.json()
+    print(name)
+    return name
+
+def get_hello_name():
+    """ makes a get request and returns the  message """
+    r = requests.get("http://vcm-3586.vm.duke.edu:5000/hello/souhaila")
+    message = r.json()
+    print(message)
+    return message
+
+def get_distance():
+    """makes a post request to the virtual machine and returns the distance"""
+    input = {
+        "a": [1,1],
+        "b": [2,2]
+         } 
+    r = requests.post("http://vcm-3586.vm.duke.edu:5000/distance", json=input)
+    distance = r.json()
+    print(distance)
+    return distance
+
+def main():
+    name = get_name()
+    message = get_hello_name()
+    distance = get_distance()
+    return name, message, distance
